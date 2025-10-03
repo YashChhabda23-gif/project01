9db76a8458a77a02e9f7d4f61881eff2ec883e53
YashChhabda23-gif <pajichhabda12345@gmail.com>
2025-10-04 02:30:03 +0530

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,1 +1,12 @@
+const db = firebase.firestore();
+
+document.getElementById('createModule').onclick = async () => {
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  await db.collection('modules').add({
+    title, content,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp()
+  });
+  alert("Module created!");
+};
 
